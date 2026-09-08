7f71df64a6c45290e9801d42075cd103eb3a26bf
Adi <adizismanovich07@gmail.com>
2026-09-08 12:02:49 +0300

    p

--- soldier.py
+++ soldier.py
@@ -1,18 +1,63 @@
 import consts
 import game_field
 
+corner=(0,0)
+
+def clean_soldier():
+    for row in range(consts.SOLDIER_ROWS):
+        for col in range(consts.SOLDIER_COLS):
+            if game_field.field()[row][col]==consts.HEAD or game_field.field()[row][col]==consts.LEGS:
+                game_field.field()[row][col] = consts.EMPTY
+
 
 def soldier(x,y):
+    clean_soldier()
+    global corner
+    corner=(x,y)
     for row in range (y,y+consts.SOLDIER_BODY_ROWS-1):
         for col in range (x,x+consts.SOLDIER_COLS):
-            game_field.field()[row][col]="HEAD"
+            if game_field.field()[row][col]==consts.EMPTY:
+                game_field.field()[row][col]=consts.HEAD
+            elif game_field.field()[row][col]==consts.MINE:
+                touch_mine()
+            elif game_field.field()[row][col]==consts.FLAG:
+                touch_flag()
+
     for col in range (x,x+consts.SOLDIER_COLS):
-        game_field.field()[y+consts.SOLDIER_BODY_ROWS][col]="LEGS"
+        game_field.field()[y+consts.SOLDIER_BODY_ROWS][col]=consts.LEGS
 
 
-def move (action)
+
+def can_move (action):
+    global corner
     if action==1:
-        for
+        if corner[1]==0:
+            return False
+    if action==2:
+        if corner[0]==0:
+            return False
+    if action==3:
+        if corner[1]==consts.SOLDIER_COLS:
+            return False
+    if action==4:
+        if corner[0]==consts.SOLDIER_BODY_ROWS:
+            return False
+    return True
+
+
+def soldier_move(action):
+    if not can_move(action):
+        return False
+    if action==1:
+        soldier(corner[0],corner[1]-1)
+    if action==2:
+        soldier(corner[0]-1,corner[1])
+    if action==3:
+        soldier(corner[0],corner[1]+1)
+    if action==4:
+        soldier(corner[0]+1,corner[1])
+
+
 
 
 
@@ -29,11 +74,7 @@ def touch_flag(): #[(0,0),(0,1)]
     pass
 
 #gets list of leg places in matrix, and list of flag places in matrix
-def touch_mine(lst_legs, lst_mine): #[(0,0),(0,1)]
-    for leg in lst_legs:
-        if leg in lst_mine:
-            return True
-    return False
+def touch_mine(): #[(0,0),(0,1)]
 
 
 
