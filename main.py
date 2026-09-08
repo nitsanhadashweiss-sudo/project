6606073d41f4ffe8698ffa6719cbf5693ac436b2
inesa <22inessa224@gmail.com>
2026-09-08 10:02:12 +0300

    i

--- main.py
+++ main.py
@@ -16,8 +16,3 @@ if __name__ == '__main__':
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
 
-def body():
-    pass
-
-def leg():
-    pass
---
+++ soldier.py
@@ -0,0 +1,20 @@
+import consts
+
+
+
+
+def body():
+    pass
+
+def leg():
+    pass
+
+def lose():
+    #legs to mine
+    pass
+
+def win():
+    #body to flag
+
+    #win message 3 seconds
+    pass
