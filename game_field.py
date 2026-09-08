6908d074e1f2435b8610c421ecfb6527abb18976
inesa <22inessa224@gmail.com>
2026-09-08 10:24:23 +0300

    i

--- game_field.py
+++ game_field.py
@@ -1,1 +1,10 @@
 import consts
+
+
+def create_fied():
+    pass
+
+def random_mines():
+    pass
+
+
--- soldier.py
+++ soldier.py
@@ -3,18 +3,18 @@ import consts
 
 
 
-def body():
+def touch_flag(): #[(0,0),(0,1)]
+    #body
     pass
 
-def leg():
-    pass
+#gets list of leg places in matrix, and list of flag places in matrix
+def touch_mine(lst_legs, lst_mine): #[(0,0),(0,1)]
+    for leg in lst_legs:
+        if leg in lst_mine:
+            return True
+    return False
 
-def lose():
-    #legs to mine
-    pass
 
-def win():
-    #body to flag
 
     #win message 3 seconds
     pass
