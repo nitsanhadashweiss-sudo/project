74c086027203347ca79a56e12adbe861e2b763a0
inesa <22inessa224@gmail.com>
2026-09-08 12:03:48 +0300

    i

--- game_field.py
+++ game_field.py
@@ -5,18 +5,57 @@ import soldier
 field=[]
 
 def create_field():
+    # global field
+    # #make the whole field empty
+    # field=[[consts.EMPTY]*consts.BOARD_COLS for i in range(consts.BOARD_ROWS)]
+    #
+    # #put soldier at start
+    # # for i in range(consts.SOLDIER_ROWS*consts.SOLDIER_COLS):
+    # for r in range(0,consts.SOLDIER_BODY_ROWS):
+    #     for c in range(0,consts.SOLDIER_FEET_ROWS):
+    #         field[r][c]=consts.HEAD
+    #
+    # for r in range(consts.SOLDIER_BODY_ROWS, consts.SOLDIER_BODY_ROWS + consts.SOLDIER_FEET_ROWS):
+    #     for c in range(0,consts.SOLDIER_FEET_ROWS):
+    #         field[r][c] = consts.LEGS
+    #
+    # # soldier.soldier(0,0)
+    #
+    # #put flag at end
+    # for i in range(consts.FLAG_ROWS*consts.FLAG_COLS):
+    #     for r in range(len(field)-1,0,-1):
+    #         for c in range(len(field[r])-1,0,-1):
+    #             if r>len(field)-1-consts.FLAG_ROWS and c>len(field[r])-1-consts.FLAG_COLS:
+    #                 field[r][c]=consts.FLAG
+
     global field
     #make the whole field empty
     field=[[consts.EMPTY]*consts.BOARD_COLS for i in range(consts.BOARD_ROWS)]
 
     #put soldier at start
-    # for i in range(consts.SOLDIER_ROWS*consts.SOLDIER_COLS):
-    #     for r in range(len(game_field)):
-    #         for c in range(len(game_field[r])):
-    #             if c<consts.SOLDIER_COLS and r<:
+    # for r in range(0,consts.SOLDIER_BODY_ROWS):
+    #     for c in range(0,consts.SOLDIER_COLS):
+    #         field[r][c]=consts.HEAD
+    #
+    # for r in range(consts.SOLDIER_BODY_ROWS, consts.SOLDIER_BODY_ROWS + consts.SOLDIER_FEET_ROWS):
+    #     for c in range(0,consts.SOLDIER_COLS):
+    #         field[r][c] = consts.LEGS
+
     soldier.soldier(0,0)
 
+    # #put flag at end
+    # for i in range(consts.FLAG_ROWS*consts.FLAG_COLS):
+    #     for r in range(len(field)-1,0,-1):
+    #         for c in range(len(field[r])-1,0,-1):
+    #             if r>len(field)-1-consts.FLAG_ROWS and c>len(field[r])-1-consts.FLAG_COLS:
+    #                 field[r][c]=consts.FLAG
 
+
+
+create_field()
+# print(field)
+for row in field:
+    print(row)
 
 
 def create_row(row_number):
@@ -32,7 +71,7 @@ def add_random_mines():
         rand_c = random.randint(0, consts.BOARD_COLS - 1)
 
         place=field[rand_r][rand_c]
-        if place!=consts.SOLDIER and place!=consts.FLAG:  # not where soldier starts and not where flag
+        if place!=consts.HEAD and place!=consts.LEGS and place!=consts.FLAG and place!=consts.MINE:  # not where soldier starts and not where flag
             field[rand_r][rand_c] = consts.MINE  # add on that place mine
             count += 1
 
