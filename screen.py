015ea6291dd29afd0cfdcb5d1a0155f72108877a
inesa <22inessa224@gmail.com>
2026-09-09 11:58:57 +0300

    sold

--- screen.py
+++ screen.py
@@ -59,6 +59,7 @@ def draw_game(field, soldier_pos, show_mines=False):
 
             # elif field[r][c]==consts.SOLDIER_IMAGE and INJURY:
             #     screen.blit(injury_img,(x, y))
+            #
 
 
 
