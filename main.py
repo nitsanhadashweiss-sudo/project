b704bbf5293b94f7953497d936c2a2cefa0c2c6a
Adi <adizismanovich07@gmail.com>
2026-09-09 11:28:41 +0300

    nn

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ see_mines=False
 game_field.create_field()
 
 while running:
-    action=1
+    action=0
 
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
@@ -39,7 +39,7 @@ while running:
 
             game_result = soldier.game_state()
 
-            if game_result:
+            if game_result!="0":
                 screen.draw_game(game_field.field, soldier.corner, see_mines)
 
                 screen.draw_message(game_result)
--- screen.py
+++ screen.py
@@ -31,10 +31,13 @@ flag_img=pygame.transform.scale(
 def draw_game(field, soldier_pos, show_mines=False):
     screen.fill(consts.GREEN)
 
+
     for r in range(consts.BOARD_ROWS):
+        print (r)
         for c in range(consts.BOARD_COLS):
             x = c * consts.CELL_SIZE
             y = r * consts.CELL_SIZE
+
 
             if field[r][c]==consts.BUSH and bush_img:
                 screen.blit(bush_img,(x, y))
@@ -45,7 +48,7 @@ def draw_game(field, soldier_pos, show_mines=False):
             elif field[r][c]==consts.SOLDIER_IMAGE and show_mines:
                 screen.blit(soldier_night_img,(x, y))
             elif field[r][c]==consts.FLAG:
-                screen.blit(flag_img,(x, y))
+                screen.blit(flag_img,(r, c))
 
             # elif r==consts.FLAG_ROWS-1 and c==consts.FLAG_COLS-1:
             #     screen.blit(flag_img, (x, y))
